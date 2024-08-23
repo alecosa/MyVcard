fd425689d0d84838a5c7cf37f1303c948a1d9a38
Hector Alejandro Parada Blanco <37299769+alecosa@users.noreply.github.com>
2024-08-23 16:06:11 -0500

    Add files via upload

---
+++ Librerias/vendorQR/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'curl/curl' => array(
+            'pretty_version' => '2.5.0',
+            'version' => '2.5.0.0',
+            'reference' => 'c4f8799c471e43b7c782c77d5c6e178d0465e210',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../curl/curl',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nguyenary/qr-code-monkey' => array(
+            'pretty_version' => 'v1.0',
+            'version' => '1.0.0.0',
+            'reference' => '77a3941f73eba25d1ef749bb75de8f5a7755c2df',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nguyenary/qr-code-monkey',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
